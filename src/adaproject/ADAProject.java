47766a607b653503840c062b8184e4d95c5b085e
SIA Miguel <SIA Miguel@Lab-SIA>
2018-02-21 23:37:29 -0600

    Commit inicial

---
+++ src/adaproject/ADAProject.java
@@ -0,0 +1,17 @@
+
+package adaproject;
+
+/**
+ *
+ * @author Miguel Moreno
+ */
+public class ADAProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
